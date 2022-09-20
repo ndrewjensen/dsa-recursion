6693e449a4e9aa0aae3e479effc9291ca0fe8884
Andrew Jensen <andrewkjensen@gmail.com>
2022-09-20 14:56:51 -0700

    complete through binary search

--- recursion.js
+++ recursion.js
@@ -1,24 +1,21 @@
+"use strict";
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-  if (nums.length === 0) {
-    debugger;
-    return 1;
-  }
-  debugger;
+  if (nums.length === 0) return 1;
   return nums[0] * product(nums.slice(1));
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-  if (words.length === 0) {
-    return 0;
-  }
+  if (words.length === 0) return 0;
 
-  return words[0].length > longest(words.slice(1))
+  return (
+    words[0].length > longest(words.slice(1))
     ? words[0].length
-    : longest(words.slice(1));
+    : longest(words.slice(1))
+  );
 }
 
 /** everyOther: return a string with every other letter. */
@@ -26,9 +23,7 @@ function longest(words) {
 function everyOther(str) {
   if (str.length === 0) return "";
   if (str.length < 2) return str[0];
-
   return str[0] + everyOther(str.slice(2));
-
 }
 
 /** find: return boolean depending on if val exists in array or not. */
@@ -42,18 +37,32 @@ function find(arr, val) {
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
-  if (str.length === 1) return str[0]
-
-  return str[0] + isPalindrome
+  if (str.length === 0) return true
+  if (str[0] !== str[str.length-1]) return false
+  return isPalindrome(str.slice(1,str.length-1))
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) {
+  if (str.length ===0) return ""
+  return revString(str.slice(1)) + str[0]
+}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
-
-function findIndex(arr, val) {}
+// ([1,2,3],2)
+function findIndex(arr, val, i=0) {
+  if (i === arr.length) {
+    debugger
+    return -1
+  }
+  if (arr[i]===val) {
+    debugger
+    return i
+  }
+  debugger
+  return findIndex(arr,val,i+1)
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
@@ -62,9 +71,28 @@ function gatherStrings(obj) {}
 // FURTHER STUDY
 
 /** binarySearch: given a sorted array of numbers, and a value,
- * return true if val is in array, false if not present). */
+ * return true if val is in array, -1 if not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr, val) {
+  if (arr.length === 0) return false
+  if (arr.length === 1 && arr[0]!==val) {
+    debugger
+    return false
+  } else if (arr[0]===val) {
+    return true
+  }
+  if (arr[Math.floor(arr.length/2)-1]===val) {
+    debugger
+    return true
+  } else if (arr[Math.floor(arr.length/2)-1] < val) {
+    debugger
+    return binarySearch(arr.slice(Math.floor(arr.length/2)),val)
+  } else {
+    debugger
+    return binarySearch(arr.slice(0,Math.floor(arr.length/2)),val)
+  }
+}
+
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
--- recursion.test.js
+++ recursion.test.js
@@ -87,6 +87,35 @@ describe("revString", function() {
   });
 });
 
+
+describe("binarySearch", function () {
+  it("should find the index of a value in a sorted array", function() {
+    expect(binarySearch([1, 2, 3, 4], 4)).toEqual(true);
+    expect(binarySearch([1, 2], 1)).toEqual(true);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(true);
+  });
+  
+  it("should return -1 if the value is not found", function() {
+    expect(binarySearch([], 0)).toEqual(false);
+    expect(binarySearch([1, 2, 3, 4], 0)).toEqual(false);
+    expect(binarySearch([1, 2], 11)).toEqual(false);
+  });
+});
+
+describe("binarySearchIndex", function () {
+  it("should find the index of a value in a sorted array", function () {
+    expect(binarySearchIndex([1, 2, 3, 4], 4)).toEqual(3);
+    expect(binarySearchIndex([1, 2], 1)).toEqual(0);
+    expect(binarySearchIndex([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(5);
+  });
+  
+  it("should return -1 if the value is not found", function () {
+    expect(binarySearchIndex([], 0)).toEqual(-1);
+    expect(binarySearchIndex([1, 2, 3, 4], 0)).toEqual(-1);
+    expect(binarySearchIndex([1, 2], 11)).toEqual(-1);
+  });
+});
+
 describe("gatherStrings", function() {
   it("gathers all object string values into an array", function() {
     let whiskey = {
@@ -119,33 +148,5 @@ describe("gatherStrings", function() {
       }
     };
     expect(gatherStrings(nestedObj).sort()).toEqual(["Lester", "Testowitz", "you made it!", "nice!"].sort());
-  });
-});
-
-describe("binarySearch", function () {
-  it("should find the index of a value in a sorted array", function() {
-    expect(binarySearch([1, 2, 3, 4], 4)).toEqual(true);
-    expect(binarySearch([1, 2], 1)).toEqual(true);
-    expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(true);
-  });
-
-  it("should return -1 if the value is not found", function() {
-    expect(binarySearch([], 0)).toEqual(false);
-    expect(binarySearch([1, 2, 3, 4], 0)).toEqual(false);
-    expect(binarySearch([1, 2], 11)).toEqual(false);
-  });
-});
-
-describe("binarySearchIndex", function () {
-  it("should find the index of a value in a sorted array", function () {
-    expect(binarySearchIndex([1, 2, 3, 4], 4)).toEqual(3);
-    expect(binarySearchIndex([1, 2], 1)).toEqual(0);
-    expect(binarySearchIndex([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(5);
-  });
-
-  it("should return -1 if the value is not found", function () {
-    expect(binarySearchIndex([], 0)).toEqual(-1);
-    expect(binarySearchIndex([1, 2, 3, 4], 0)).toEqual(-1);
-    expect(binarySearchIndex([1, 2], 11)).toEqual(-1);
   });
 });
